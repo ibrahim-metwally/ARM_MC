67bf2e64232060c17de6664cadf7ea32445821ca
Ibrahim Metwally <40437076+ibrahim-metwally@users.noreply.github.com>
2021-02-20 15:17:54 +0200

    Delete Build.bat

--- Build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-arm-none-eabi-gcc -mcpu=cortex-m4 -march=armv7e-m -mthumb -save-temps -nostdlib -O0 -Wl,-Map,"App_Main.map" -Wl,-T"tm4c123gh6pm.lds" ../MCAL/CPU/CPU.c startupCode.c ../Application/app_main.c -o ../Out/Exe/app_Out.elf
-pause
