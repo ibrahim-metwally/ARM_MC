8c4daa7add4d34b8bfecd128c146b6b19b62182d
Ibrahim Metwally <40437076+ibrahim-metwally@users.noreply.github.com>
2021-02-20 16:47:26 +0200

    Update Build File

--- Workspace/Out/Build.bat
+++ Workspace/Out/Build.bat
@@ -1,3 +1,3 @@
 @echo off
-arm-none-eabi-gcc -mcpu=cortex-m4 -march=armv7e-m -mthumb -save-temps -nostdlib -O0 -Wl,-Map,"App_Main.map" -Wl,-T"tm4c123gh6pm.lds" ../MCAL/CPU/CPU.c startupCode.c ../Application/app_main.c -o ../Out/Exe/app_Out.elf
+arm-none-eabi-gcc -mcpu=cortex-m4 -march=armv7e-m -mthumb -save-temps -nostdlib -O0 -Wl,-Map,"App_Main.map" -Wl,-T"tm4c123gh6pm.lds" ../MCAL/CPU/CPU.c startupCode.c ../Application/app_main.c -o Exe/app_Out.elf
 pause
